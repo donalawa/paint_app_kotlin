23016eaef5a1183929a49d87c07e2fd5386c7c5b
donalawa <awadonalcien12@gmail.com>
2021-09-05 14:27:25 -0700

    Added brush thickness function

--- app/src/main/java/com/example/androidpaint/MainActivity.kt
+++ app/src/main/java/com/example/androidpaint/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.androidpaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        We want to call the setSizeForBrush function here in our main and pass the brush size
+        drawing_view.setSizeForBrush(20.toFloat())
     }
 }
